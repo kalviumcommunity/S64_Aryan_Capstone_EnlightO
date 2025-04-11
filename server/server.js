211e637b64aac9ae243b630f2e4009408016af80
Aryan Raj <adityaraj@Adityas-MacBook-Air.local>
2025-04-11 12:42:07 +0530

    backend setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require ('express');
+const cors = require('cors');
+
+const app= express();
+const PORT = 5000;
+
+app.use((err,req,res,next)=> {
+    console.log(err.stack);
+    res.status(500).json({
+        success : false,
+        message : 'Something went wrong'
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is now running on port ${PORT}`)
+})
